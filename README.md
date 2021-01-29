a57669ed82ea569405b7f1b4087a79aad9ebaf92
Robby Noyes <robertjnoyes1@gmail.com>
2021-01-29 11:39:49 -0800

    Created outline for project in main.js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-A-Sketch
+
+# Create an etch-a-sketch based on divs and the hover eventlistener.  
+
+## Mainly trying to work in JS specifically to manipulate the DOM
---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*
+
+Make a series of divs, 16 x 16 
+    -use DOM manipulation to create divs in row
+    -make rows same length as width
+
+Use mouse hover to manipulate color of divs
+    -make background color change from white to black
+    -
+
+Add button to make divs clear after changing color, ask user for size
+    -eventlistener waiting for click to make divs change color
+
+    -prompt connected to square amount to change squares 
+
+
+*/
+
