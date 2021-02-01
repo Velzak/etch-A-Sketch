4058e65a9968b387d8f1caa624d29da1a459c189
Robby Noyes <robertjnoyes1@gmail.com>
2021-02-01 14:26:46 -0800

    main etch-a-sketch complete, size change and sketch working

--- js/main.js
+++ js/main.js
@@ -19,10 +19,10 @@ askUser.id = "clear";
 askUser.textContent = "Clear";
 upperDiv.appendChild(askUser);
 
-let colorPicker = document.createElement("button");
-colorPicker.id = "colorpicker";
-colorPicker.textContent = "Change Color";
-upperDiv.appendChild(colorPicker);
+// let colorPicker = document.createElement("button");
+// colorPicker.id = "colorPicker";
+// colorPicker.textContent = "Change Color";
+// upperDiv.appendChild(colorPicker);
 
 let boxDiv = document.createElement("div");
 boxDiv.id = "outerBox";
@@ -30,46 +30,47 @@ document.body.appendChild(boxDiv);
 
 //Creates divs based on userInput
 function createDiv(userInput) {
-  let smallDiv = document.createElement("div");
-  smallDiv.classList.add("innerBox");
-  smallDiv.classList.add("container");
+    let smallDiv = document.createElement("div");
+    smallDiv.classList.add("innerBox");
 
-  boxDiv.appendChild(smallDiv);
-  let sum;
-  if (userInput === undefined) {
-    sum = 798 / 16;
-  } else {
-    sum = 798 / Number(userInput);
-  }
+    boxDiv.appendChild(smallDiv);
+    let sum;
+    if (userInput === undefined) {
+        sum = 798 / 16;
+    } else {
+        sum = 798 / Number(userInput);
+    }
 
-  smallDiv.style.height = sum + "px";
-  smallDiv.style.width = sum + "px";
-  smallDiv.style.background = "white";
-  smallDiv.addEventListener("mouseenter", () => {
-    smallDiv.style.background = "black";
-  });
+    smallDiv.style.height = sum + "px";
+    smallDiv.style.width = sum + "px";
+    smallDiv.style.background = "white";
+    smallDiv.addEventListener("mouseenter", () => {
+        smallDiv.style.background = "black";
+    });
+
+
 }
 
 //Creates the rows and columns of divs based on userinput, or default start of 16
 function createGrid(userInput) {
-  let width = userInput;
-  let height = userInput - 1;
+    let width = userInput;
+    let height = userInput - 1;
 
-  if (userInput === undefined) {
-    for (let i = 0; i < 16; i++) {
-      for (let x = 0; x < 15; x++) {
-        createDiv();
-      }
-      createDiv();
+    if (userInput === undefined) {
+        for (let i = 0; i < 16; i++) {
+            for (let x = 0; x < 15; x++) {
+                createDiv();
+            }
+            createDiv();
+        }
+    } else {
+        for (let i = 0; i < width; i++) {
+            for (let x = 0; x < height; x++) {
+                createDiv(userInput);
+            }
+            createDiv(userInput);
+        }
     }
-  } else {
-    for (let i = 0; i < width; i++) {
-      for (let x = 0; x < height; x++) {
-        createDiv(userInput);
-      }
-      createDiv(userInput);
-    }
-  }
 }
 //if nothing clicked, rows and columsn runs default, otherwise run with prompt return
 createGrid();
@@ -80,26 +81,30 @@ getClear.addEventListener("click", gridClear);
 
 //function allows for the color of the square divs to turn back to white
 function gridClear() {
-  innerBox.forEach((element) => {
-    element.style.background = "white";
-  });
-  //TODO:  Create prompt that will ask user for new input
+    innerBox.forEach((element) => {
+        element.style.background = "white";
+    });
 
-  let userInput = prompt("enter a new grid size:");
-  if (userInput <= 100 && userInput >= 1) {
-    clearGrid();
-    createGrid(userInput);
-  } else {
-    alert("Please Enter a Number from 1 - 100");
-  }
+    let userInput = prompt("enter a new grid size:");
+    if (userInput <= 100 && userInput >= 1) {
+        clearGrid();
+        createGrid(userInput);
+    } else {
+        alert("Please Enter a Number from 1 - 100");
+    }
 }
 
 function clearGrid() {
-  let elements = document.getElementsByClassName("innerBox");
-  while (elements.length > 0) {
-    elements[0].parentNode.removeChild(elements[0]);
-  }
+    let elements = document.getElementsByClassName("innerBox");
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
 }
 
-//Allows the mouse to hover and change background color for the divs
-let innerBox = document.querySelectorAll(".container");
+let innerBox = document.querySelectorAll(".innerBox");
+
+
+//Create button to change color to random 
+
+
+
